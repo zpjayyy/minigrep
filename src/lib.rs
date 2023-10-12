f188ba2df678d6fb9b6aacdcee9b968e872e46a5
zpjayyy <jay.lichzp@gmail.com>
2023-10-12 14:48:16 +0800

    feat: mini grep

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+
 use std::{env, fs};
 use std::error::Error;
-use std::ptr::eq;
 
+/// search string in a file
+///
+/// # Examples
+///
+/// ```
+///    if let Err(e) = minigrep::run(config) {
+///         eprintln!("Application error: {e}");
+///         exit(1);
+///     }
+/// ```
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
@@ -19,48 +38,48 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = vec![];
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = vec![];
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
-    pub ignore_case: bool
+    pub ignore_case: bool,
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item=String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("not get query string")
+        };
+
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("not get file path")
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
             query,
             file_path,
-            ignore_case
+            ignore_case,
         })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use minigrep::Config;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args)
+    let config = Config::build(env::args())
         .unwrap_or_else(|err| {
             eprintln!("Problem parsing arguments: {err}");
             exit(1);
